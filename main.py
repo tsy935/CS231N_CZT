f6edbb46fb9e3d8be5a56cd1791f47a2ce339597
Ruge Zhao <rugezhao@gmail.com>
2019-06-02 21:04:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -446,7 +446,7 @@ def evaluate(model, args, test_save_dir, device, is_test=False, write_outputs=Fa
         df_out.to_csv(out_file_name, index=False)
         print('Prediction written to {}!'.format(out_file_name))
 
-        df_y_pred_raw = pd.DataFrame(y_pred_all)  
+        df_y_pred_raw = pd.DataFrame(y_pred_raw)  
         out_file_name_raw = os.path.join(test_save_dir,args.split+'_prediction_raw_prob.csv')
         df_y_pred_raw.to_csv(out_file_name_raw, index=False)
         print('Probability Prediction written to {}!'.format(out_file_name_raw))      
