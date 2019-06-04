35c02452d21119ff915c6b896fd4c10b04114850
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-04 03:13:52 +0000

    Add script for hog+cnn-rnn prediction

---
+++ scripts/run_hog_cnnrnn_val.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ..
+
+python ./main.py \
+  --do_predict \
+  --split test \
+  --use_pretrained \
+  --model_name hog_cnn-rnn \
+  --loss_fn_name BCE \
+  --write_outputs \
+  --metric_avg samples \
+  --feature_extracting \
+  --prob_path_thresh 2.5 \
+  --beam_search \
+  --beam_size 5 \
+  --load_path /mnt/disks/large/output/hog_cnnrnn/train/train-04/best.pth.tar \
+  --best_val_results /mnt/disks/large/output/hog_cnnrnn/train/train-04/best_val_results \
+  --save_dir /mnt/disks/large/output/hog_cnnrnn
